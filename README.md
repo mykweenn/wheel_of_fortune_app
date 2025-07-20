c5e893d75a4479c3e6389063c7491cd39eb344d3
Sindikaty <vlad2000feoktistov@mail.ru>
2025-07-20 17:53:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wheel_of_fortune_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/wheel_of_fortune_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wheel_of_fortune_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
